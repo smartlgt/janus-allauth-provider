5f67642eea3ba989d469e1c6cf618045e21c66e9
Daniel Leinfelder <daniel@smart-lgt.com>
2019-01-31 14:35:43 +0100

    switch user model

--- allauth_janus/adapter.py
+++ allauth_janus/adapter.py
@@ -1,15 +1,13 @@
-from django.contrib.auth.models import User
-
-from allauth.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth import get_user_model
 
 
 class Adapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         # This isn't tested, but should work
         try:
-            user = User.objects.get(username=sociallogin.account.uid)
+            user = get_user_model().objects.get(username=sociallogin.account.uid)
             if not sociallogin.is_existing:
                 sociallogin.connect(request, user)
-        except User.DoesNotExist:
+        except get_user_model().DoesNotExist:
             pass
