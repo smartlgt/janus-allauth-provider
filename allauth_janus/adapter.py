943e62a77817f430e1a4a1e5b5ab1b28b5594df2
Daniel Leinfelder <daniel@smart-lgt.com>
2019-02-01 02:33:42 +0100

    add group permission transfer

--- allauth_janus/adapter.py
+++ allauth_janus/adapter.py
@@ -19,6 +19,37 @@ class Adapter(DefaultSocialAccountAdapter):
         # populate the extra data to the user on every login,
         # DO IT HERE, populate_user has only a fake user object
 
+        # permissions
         user.is_superuser = extra_data.get('is_superuser', False)
+        if user.is_superuser:
+            user.is_staff = True # also allow superuser to log into the admin panel
+
+        groups = extra_data.get('groups', [])
+
+        # do this more carefully, only remove user from group if not present anymore
+        # not remove all and add them all again
+
+        for gg in user.groups.all():
+            if gg.name not in groups:
+                user.groups.remove(gg)
+
+        for group in groups:
+            from django.contrib.auth.models import Group
+            try:
+                group_object = Group.objects.get(name=group)
+                user.groups.add(group_object)
+            except Group.DoesNotExist as e:
+                # log this but don't raise an exception
+                pass
+
+        # user data
+        user.first_name = extra_data.get('first_name')
+        user.last_name = extra_data.get('last_name')
+
+        # sync email via user allauth class
+        #user.email = extra_data.get('email')
+
+        #verified = True,
+        #primary = True
 
         user.save()
