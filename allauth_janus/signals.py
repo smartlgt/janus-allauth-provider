6f5f6ec70d60cb21872c89be27155e556b0fca0a
Daniel Leinfelder <daniel@smart-lgt.com>
2019-07-31 04:19:32 +0200

    add remote logout

--- allauth_janus/signals.py
+++ allauth_janus/signals.py
@@ -1,14 +1,18 @@
+import requests
+from django.conf import settings
 from django.dispatch import receiver
-from allauth.account.signals import user_signed_up
+from allauth.account.signals import user_signed_up, user_logged_out
 from allauth.socialaccount.signals import social_account_updated
 
 from allauth_janus.helper import janus_sync_user_properties
+
 
 @receiver(social_account_updated)
 def social_account_updated(sender, request, sociallogin, **kwargs):
 
     if sociallogin.account.provider == "janus":
         janus_sync_user_properties(request, sociallogin)
+
 
 @receiver(user_signed_up)
 def user_signed_up(sender, request, user, **kwargs):
@@ -19,3 +23,26 @@ def user_signed_up(sender, request, user, **kwargs):
         janus_sync_user_properties(request, sociallogin)
 
 
+@receiver(user_logged_out)
+def user_logged_out(sender, request, user, **kwargs):
+    """
+    send a logout command to the sso janus instance to ensure no token or session is valid
+    if the user return for authorisation
+    """
+
+    from allauth.socialaccount.models import SocialToken
+    token = SocialToken.objects.filter(account__user=user, account__provider='janus').first()
+
+    remote_logout = bool(settings.get('ALLAUTH_REMOTE_LOGOUT', False))
+
+    if remote_logout and token:
+        url = settings.ALLAUTH_JANUS_URL + '/o/logout/'
+        try:
+            response = requests.get(
+                url,
+                params={'access_token': token}
+            )
+            if response.content == "OK":
+                pass
+        except Exception as e:
+            pass
