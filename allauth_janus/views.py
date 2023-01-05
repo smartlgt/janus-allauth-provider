0489d0e7c5f09c4f8cd81cff8f254521a28416e5
Julian Mundhahs <Qup42@users.noreply.github.com>
2023-01-05 13:23:54 +0100

    Use Authorization Request Header for Token

--- allauth_janus/views.py
+++ allauth_janus/views.py
@@ -21,7 +21,7 @@ class JanusOAuth2Adapter(OAuth2Adapter):
     def complete_login(self, request, app, token, **kwargs):
         response = requests.get(
             self.profile_url,
-            params={'access_token': token})
+            headers={"Authorization": f"Bearer {token}"})
         extra_data = response.json()
 
         return self.get_provider().sociallogin_from_response(
